78d06250ff60fb81227b2f411d78087511975180
Jone212 <15651342081@163.com>
2017-01-22 12:47:11 +0800

    requestHelper

--- smart-framework/src/main/java/helper/Request.java
+++ smart-framework/src/main/java/helper/Request.java
@@ -4,12 +4,14 @@ package helper;
  * Project: simpleApp
  * Author: Chow xi
  * Email: zhouxu_1994@163.com
- * Time: 17/1/22 下午12:39
+ * Time: 17/1/22 下午12:46
  */
-public class ControllerHelper {
+public class Request {
 
 
-
+    /**
+     * 封装 请求对象
+     */
 
 
 }
